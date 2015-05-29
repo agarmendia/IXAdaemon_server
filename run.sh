46ba9831a12dcf275dad3935cb9738a749c656b6
agarmendia <aitzolgarmendia@gmail.com>
2015-05-29 17:38:09 +0200

    Changed:
      - run.sh script has been modified. It now manages inusual situations, like having instances running without a server to control them.

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ NATIVE="atzerapena"
 clear
 serveIP_ctrl
 if [ $? == "3" ]; then
+	n=`ps aux | grep atzerapena | grep java | wc -l`
+	if (($n > "0")); then
+		nativeProcesses=(`ps aux | grep atzerapena | pidof java`)
+		
+		for (( i=0; i<$n; i++ ))
+		do
+			kill ${nativeProcesses[$i]}
+		done
+	fi
 	serveIP_server $LANGUAGE $NATIVE &
 fi
 
@@ -19,7 +28,6 @@ if [ $STATE == "2" ]; then
 
 fi
 
-echo -n "waiting for initialization..."
 while [ $STATE -ne "0" ]; do
 	echo -n "..."
 	sleep 1
